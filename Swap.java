e67a00148cf4a5ac110e2050d692f4531b02c35d
lavanya616 <lakshmilavanya1802@gmail.com>
2024-10-24 11:32:51 +0530

    Update Swap.java

--- Swap.java
+++ Swap.java
@@ -1,39 +1,35 @@
 public class GenericSwap {
-    // Generic swap method
     public static <T> void swap(T[] array) {
         if (array == null || array.length < 2) {
-            return; // No swap needed if array is null or has less than two elements
+            return; 
         }
-        
-        // Swap the first and last elements
+       
         T temp = array[0];
         array[0] = array[array.length - 1];
         array[array.length - 1] = temp;
     }
 
     public static void main(String[] args) {
-        // Sample usage with an Integer array
-        Integer[] intArray = {1, 2, 3, 4, 5};
+        Integer[] intArray = {7,6,5,4,3};
         System.out.println("Before swap: ");
         for (int num : intArray) {
             System.out.print(num + " ");
         }
         
-        swap(intArray); // Perform swap
+        swap(intArray); 
         
         System.out.println("\nAfter swap: ");
         for (int num : intArray) {
             System.out.print(num + " ");
         }
 
-        // Sample usage with a String array
-        String[] strArray = {"apple", "banana", "cherry"};
+        String[] strArray = {"tiger", "lion", "pearl"};
         System.out.println("\n\nBefore swap: ");
         for (String str : strArray) {
             System.out.print(str + " ");
         }
         
-        swap(strArray); // Perform swap
+        swap(strArray); 
         
         System.out.println("\nAfter swap: ");
         for (String str : strArray) {
